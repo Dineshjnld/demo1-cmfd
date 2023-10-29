36157116b42124192af1cc077d337f0195eb3f45
Dineshjnld <dineshjnld22@gmail.com>
2023-10-29 11:33:07 +0530

    ij

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import cv2
+from PIL import Image
 import numpy as np
 from tensorflow.keras.models import load_model
 from io import BytesIO
@@ -7,8 +7,8 @@ from io import BytesIO
 model = load_model("model1.h5")
 
 def preprocess_uploaded_image(uploaded_image):
-    uploaded_image = cv2.resize(uploaded_image, (224, 224))
-    uploaded_image = uploaded_image.astype(np.float32) / 255.0
+    uploaded_image = uploaded_image.resize((224, 224))
+    uploaded_image = np.array(uploaded_image) / 255.0
     return uploaded_image
 
 def predict_image(authenticity_probability):
@@ -20,22 +20,17 @@ def predict_image(authenticity_probability):
 
 st.title("Copy-Move Forgery Detection")
 
-
 uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 if uploaded_image is not None:
-    
     st.image(uploaded_image, caption="Uploaded Image", use_column_width=True)
 
     # Read the uploaded image using BytesIO
     image_data = uploaded_image.read()
     
-    # Convert image data to a NumPy array
-    nparr = np.frombuffer(image_data, np.uint8)
+    # Open the image with Pillow
+    uploaded_image = Image.open(BytesIO(image_data))
     
-    # Decode the NumPy array to an OpenCV image
-    uploaded_image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
     # Preprocess the uploaded image
     preprocessed_image = preprocess_uploaded_image(uploaded_image)
     
